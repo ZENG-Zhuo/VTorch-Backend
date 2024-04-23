0a49f4bfebcb5241f1baf83fd7d374d3722aa79a
zzengan <zzengan@connect.ust.hk>
2024-04-23 12:40:17 +0800

    add codgen router

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import { loginRoute } from "./loginRoute";
 import { codeParseRoute } from "./codeParseRoute";
-import { codeGenRouter } from "./codeGenRouter";
 import { datasetsRouter } from "./datasetsRouter";
 import { UDBRouter } from "./UDBRouter";
 import { codeGenRouter } from "./codeGenRouter";
@@ -10,7 +9,6 @@ export const routes = Router();
 
 routes.use(loginRoute);
 routes.use(codeParseRoute);
-routes.use(codeGenRouter);
 routes.use(datasetsRouter);
 routes.use(UDBRouter);
 routes.use(codeGenRouter);
